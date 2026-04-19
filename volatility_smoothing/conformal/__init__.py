5f62ec9a3022e139cefe67e1114f2fc419f84648
Aniket Prakash Nighot <113236372+aniketnighot@users.noreply.github.com>
2026-04-19 22:42:38 +0200

    Updated comments

--- volatility_smoothing/conformal/__init__.py
+++ volatility_smoothing/conformal/__init__.py
@@ -3,11 +3,8 @@ Conformal prediction for implied volatility surfaces.
 
 Components:
 - nonconformity_scores: Score computation (spread and Vega normalized)
-- mondrian: Spatial binning for heterogeneous uncertainty
 - rolling_window: Temporal calibration window
 - prediction_bands: Uncertainty band construction
-- no_arbitrage_projection: Arbitrage constraint enforcement
-- conformal_predictor: Main prediction interface
 """
 
 from .nonconformity_scores import (
